63ab3b6a3a8537d05dd7da3cec0f7699bf9a0383
Simon Cragg <simoncragg@icloud.com>
2023-03-18 23:18:55 +0000

    chore: remove flowbite-react

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
 	darkMode: "class",
-	content: [
-		"./src/**/*.{js,jsx,ts,tsx}",
-		"./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-	],
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {},
 	},
-	plugins: [require("flowbite/plugin")],
+	plugins: [],
 };
