0b621a7c18dd246eca26c1d7c6451912175d844f
Simon Cragg <simoncragg@icloud.com>
2023-03-27 00:11:25 +0100

    feat: add flowbite-react

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
 	darkMode: "class",
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	content: [
+		"./src/**/*.{js,jsx,ts,tsx}",
+		"node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require("flowbite/plugin")],
 };
