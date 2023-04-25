0e03232ca585e1e36f3e394a131940a0aaf5a097
Simon Cragg <simoncragg@icloud.com>
2023-04-25 18:46:02 +0100

    refactor: extract more code into methods

--- api/src/services/UserMessageProcessor.ts
+++ api/src/services/UserMessageProcessor.ts
@@ -21,55 +21,31 @@ export default class UserMessageProcessor {
 		userId,
 		message,
 	}: ProcessUserMessagePayload) {
+
 		const chat = await this.getChatAsync(chatId, userId);
 		chat.messages.push(message);
 
-		const assistantMessage = await this.generateAssistantMessageAsync(chat);
-		await postToConnectionAsync(connectionId, {
-			type: "assistantMessage" as const,
-			payload: {
-				chatId,
-				message: assistantMessage
-			} as AssistantMessagePayload
-		} as WebSocketMessage);
-
-		const transcript = assistantMessage.content.replace(/```[\s\S]*?```/g, "");
-		const audioUrl = await this.textToSpeechService.generateSignedAudioUrlAsync(transcript);
-		await postToConnectionAsync(connectionId, {
-			type: "assistantAudio" as const,
-			payload: {
-				chatId,
-				transcript,
-				audioUrl
-			} as AssistantAudioPayload
-		} as WebSocketMessage);
-
-		chat.messages.push(assistantMessage);
-		chat.updatedTime = Date.now();
-		await this.chatRepository.updateItemAsync(chat);
-	}
-
-	private async generateAssistantMessageAsync(
-		chat: Chat
-	): Promise<ChatMessage> {
-		const { content } = await generateChatResponseAsync(
-			[
-				{
-					role: "system" as const,
-					content: prePrompt,
-				},
-				...chat.messages.map(msg => {
-					return { role: msg.role, content: msg.content };
-				})
-			]
+		const assistantMessage = await this.generateAssistantMessageAsync(
+			chat
 		);
 
-		return {
-			id: uuidv4(),
-			role: "assistant" as const,
-			content,
-			timestamp: Date.now(),
+		chat.messages.push(assistantMessage);
+
+		const assistantMessagePayload = {
+			chatId,
+			message: assistantMessage
 		};
+
+		await this.postAssistantMessagePayloadAsync(
+			connectionId, assistantMessagePayload
+		);
+
+		await this.processAssistantVoiceAsync(
+			connectionId,
+			assistantMessagePayload
+		);
+
+		await this.updateChatAsync(chat);
 	}
 
 	private async getChatAsync(chatId: string, userId: string): Promise<Chat> {
@@ -81,5 +57,70 @@ export default class UserMessageProcessor {
 			createdTime: Date.now(),
 			updatedTime: Date.now(),
 		};
+	}
+
+	private async generateAssistantMessageAsync(
+		chat: Chat
+	): Promise<ChatMessage> {
+		const { content } = await generateChatResponseAsync(
+			[
+				{
+					role: "system",
+					content: prePrompt,
+				},
+				...chat.messages.map(msg => {
+					return { role: msg.role, content: msg.content };
+				})
+			]
+		);
+
+		return {
+			id: uuidv4(),
+			role: "assistant",
+			content,
+			timestamp: Date.now(),
+		};
+	}
+
+	private async postAssistantMessagePayloadAsync(
+		connectionId: string,
+		payload: AssistantMessagePayload) {
+		await postToConnectionAsync(connectionId, {
+			type: "assistantMessage" as const,
+			payload,
+		} as WebSocketMessage);
+	}
+
+	private async processAssistantVoiceAsync(
+		connectionId: string,
+		{
+			chatId,
+			message,
+		}: AssistantMessagePayload) {
+
+		const transcript = message
+			.content
+			.replace(/```[\s\S]*?```/g, "");
+
+		if (transcript.length === 0) {
+			return;
+		}
+
+		const audioUrl = await this.textToSpeechService
+			.generateSignedAudioUrlAsync(transcript);
+
+		await postToConnectionAsync(connectionId, {
+			type: "assistantAudio" as const,
+			payload: {
+				chatId,
+				transcript,
+				audioUrl
+			} as AssistantAudioPayload
+		} as WebSocketMessage);
+	}
+
+	private async updateChatAsync(chat: Chat) {
+		chat.updatedTime = Date.now();
+		await this.chatRepository.updateItemAsync(chat);
 	}
 }
