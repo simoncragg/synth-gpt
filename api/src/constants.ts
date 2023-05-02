91200a5e18313ee0bcafa9e94ef5b69e3cf1b026
Simon Cragg <simoncragg@icloud.com>
2023-05-02 22:56:52 +0100

    feat: include current date in system prompt

--- api/src/constants.ts
+++ api/src/constants.ts
@@ -1,2 +1,5 @@
 export const newChatText = "New chat";
-export const prePrompt = "Your name is Synth.";
+export const prePrompt = [
+	"Your name is Synth.",
+	`The current date is ${new Date().toDateString()}`,
+].join("\n\n");
