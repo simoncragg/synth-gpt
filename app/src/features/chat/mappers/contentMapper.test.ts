03dad914cdbf19b4f6b79aaf5941618c53cd79c0
Simon Cragg <simoncragg@icloud.com>
2023-05-06 20:41:12 +0100

    feat: improved message part mapping

--- app/src/features/chat/mappers/contentMapper.test.ts
+++ app/src/features/chat/mappers/contentMapper.test.ts
@@ -42,196 +42,236 @@ describe("mapToContentParts", () => {
 		);
 	});
 
-	it("should map a paragraph followed by an ordered list", () => {
-		const content = [
-			"Sure, here are three numbered points:",
-			"1. List item one. First sentence.",
-			"2. List item two. Second sentence.",
-			"3. List item three. Third sentence.",
-		].join("\n");
+	it.each(["\n", " "])(
+		"should map a paragraph followed by an ordered list (separated by %p)",
+		(separator: string) => {
+			const content = [
+				"Sure, here are three numbered points:",
+				"1. List item one. First sentence.",
+				"2. List item two. Second sentence.",
+				"3. List item three. Third sentence.",
+			].join(separator);
 
-		const result = mapToContentParts(content);
+			const result = mapToContentParts(content);
 
-		expect(result).toEqual([
-			expect.objectContaining({
-				type: "Paragraph",
-				text: "Sure, here are three numbered points:",
-			}),
-			expect.objectContaining({
-				type: "OrderedList",
-				listItems: [
-					expect.objectContaining({
-						id: mockedId,
-						text: "1. List item one. First sentence.",
-					}),
-					expect.objectContaining({
-						id: mockedId,
-						text: "2. List item two. Second sentence.",
-					}),
-					expect.objectContaining({
-						id: mockedId,
-						text: "3. List item three. Third sentence.",
-					}),
-				],
-			}),
-		]);
-	});
-
-	it("should map a paragraph followed by an ordered list, followed by another paragraph", () => {
-		const content = [
-			"Sure, here are three numbered points:",
-			"1. List item one. First sentence.",
-			"2. List item two. Second sentence.",
-			"3. List item three. Third sentence.",
-			"Let me know if there's anything else I can help with",
-		].join("\n");
-
-		const result = mapToContentParts(content);
-
-		expect(result).toEqual([
-			expect.objectContaining({
-				type: "Paragraph",
-				text: "Sure, here are three numbered points:",
-			}),
-			expect.objectContaining({
-				type: "OrderedList",
-				listItems: [
-					expect.objectContaining({
-						id: mockedId,
-						text: "1. List item one. First sentence.",
-					}),
-					expect.objectContaining({
-						id: mockedId,
-						text: "2. List item two. Second sentence.",
-					}),
-					expect.objectContaining({
-						id: mockedId,
-						text: "3. List item three. Third sentence.",
-					}),
-				],
-			}),
-			expect.objectContaining({
-				type: "Paragraph",
-				text: "Let me know if there's anything else I can help with",
-			}),
-		]);
-	});
-
-	it("should map a paragraph followed by a code snippet", () => {
-		const content = [
-			"Sure, here is the python code:",
-			"```python",
-			"print(\"Hello, world!\")",
-			"```",
-		].join("\n");
-
-		const result = mapToContentParts(content);
-
-		expect(result).toEqual([
-			expect.objectContaining({
-				type: "Paragraph",
-				text: "Sure, here is the python code:",
-			}),
-			expect.objectContaining({
-				type: "CodeSnippet",
-				language: "python",
-				code: "print(\"Hello, world!\")",
-			}),
-		]);
-	});
-
-	it("should map a paragraph followed by a code snippet, followed by another paragraph", () => {
-		const content = [
-			"Sure, here is the python code:",
-			"```python",
-			"print(\"Hello, world!\")",
-			"```",
-			"Final Paragraph.",
-		].join("\n");
-
-		const result = mapToContentParts(content);
-
-		expect(result).toEqual([
-			expect.objectContaining({
-				type: "Paragraph",
-				text: "Sure, here is the python code:",
-			}),
-			expect.objectContaining({
-				type: "CodeSnippet",
-				language: "python",
-				code: "print(\"Hello, world!\")",
-			}),
-			expect.objectContaining({
-				type: "Paragraph",
-				text: "Final Paragraph.",
-			}),
-		]);
-	});
-
-	it("should map multiple paragraphs and code snippets", () => {
-		const content = [
-			"Sure, here is the javascript code:",
-			"```javascript",
-			"print(\"Line one\")",
-			"print(\"Line two\")",
-			"```",
-			"Here is the typescript code:",
-			"```typescript",
-			"print(\"Line A\")",
-			"print(\"Line B\")",
-			"```",
-			"Final paragraph.",
-		].join("\n");
-
-		const result = mapToContentParts(content);
-
-		expect(result).toEqual([
-			expect.objectContaining({
-				type: "Paragraph",
-				text: "Sure, here is the javascript code:",
-			}),
-			expect.objectContaining({
-				type: "CodeSnippet",
-				language: "javascript",
-				code: "print(\"Line one\")\nprint(\"Line two\")",
-			}),
-			expect.objectContaining({
-				type: "Paragraph",
-				text: "Here is the typescript code:",
-			}),
-			expect.objectContaining({
-				type: "CodeSnippet",
-				language: "typescript",
-				code: "print(\"Line A\")\nprint(\"Line B\")",
-			}),
-			expect.objectContaining({
-				type: "Paragraph",
-				text: "Final paragraph.",
-			}),
-		]);
-	});
-
-	it("should clean up erroneous code markers", () => {
-		const content =
-			"Sure! Here is a poem about Spring:\n```language\nThe sun warms the earth\nGreen buds burst forth in rebirth\n``` w";
-
-		const result = mapToContentParts(content);
-
-		expect(result).toEqual(
-			expect.arrayContaining([
+			expect(result).toEqual([
 				expect.objectContaining({
 					type: "Paragraph",
-					text: "Sure! Here is a poem about Spring:",
+					text: "Sure, here are three numbered points:",
+				}),
+				expect.objectContaining({
+					type: "OrderedList",
+					listItems: [
+						expect.objectContaining({
+							id: mockedId,
+							text: "1. List item one. First sentence.",
+						}),
+						expect.objectContaining({
+							id: mockedId,
+							text: "2. List item two. Second sentence.",
+						}),
+						expect.objectContaining({
+							id: mockedId,
+							text: "3. List item three. Third sentence.",
+						}),
+					],
+				}),
+			]);
+		}
+	);
+
+	it.each(["\n", " "])(
+		"should map a paragraph, an ordered list, and another paragraph (separated by %p)",
+		(separator: string) => {
+			const content = [
+				"Sure, here are three numbered points:",
+				"1. List item one. First sentence.",
+				"2. List item two. Second sentence.",
+				"3. List item three. Third sentence.\nLet me know if there's anything else I can help with",
+			]
+				.join(separator)
+				.trim();
+
+			const result = mapToContentParts(content);
+
+			expect(result).toEqual([
+				expect.objectContaining({
+					type: "Paragraph",
+					text: "Sure, here are three numbered points:",
+				}),
+				expect.objectContaining({
+					type: "OrderedList",
+					listItems: [
+						expect.objectContaining({
+							id: mockedId,
+							text: "1. List item one. First sentence.",
+						}),
+						expect.objectContaining({
+							id: mockedId,
+							text: "2. List item two. Second sentence.",
+						}),
+						expect.objectContaining({
+							id: mockedId,
+							text: "3. List item three. Third sentence.",
+						}),
+					],
 				}),
 				expect.objectContaining({
 					type: "Paragraph",
-					text: "The sun warms the earth",
+					text: "Let me know if there's anything else I can help with",
+				}),
+			]);
+		}
+	);
+
+	it.each(["\n", " "])(
+		"should map a code snippet (separated by %p)",
+		(separator: string) => {
+			const content = ["```typescript", "print('Hello, world!')", "```"].join(
+				separator
+			);
+
+			const result = mapToContentParts(content);
+
+			expect(result).toEqual([
+				expect.objectContaining({
+					type: "CodeSnippet",
+					language: "typescript",
+					code: "print('Hello, world!')\n",
+				}),
+			]);
+		}
+	);
+
+	it.each(["\n", " "])(
+		"should map a paragraph followed by a code snippet (separated by %p)",
+		(separator: string) => {
+			const content = [
+				"Sure, here is the jsx code:",
+				"```jsx",
+				"<HelloWorld />",
+				"```",
+			].join(separator);
+
+			const result = mapToContentParts(content);
+
+			expect(result).toEqual([
+				expect.objectContaining({
+					type: "Paragraph",
+					text: "Sure, here is the jsx code:",
+				}),
+				expect.objectContaining({
+					type: "CodeSnippet",
+					language: "jsx",
+					code: "<HelloWorld />\n",
+				}),
+			]);
+		}
+	);
+
+	it.each(["\n", " "])(
+		"should map a paragraph, code snippet, and another paragraph (separated by %p)",
+		(separator: string) => {
+			const content = [
+				"Sure, here is the javascript code:",
+				"```javascript",
+				"print('Hello, world!')",
+				"```",
+				"Final Paragraph.",
+			].join(separator);
+
+			const result = mapToContentParts(content);
+
+			expect(result).toEqual([
+				expect.objectContaining({
+					type: "Paragraph",
+					text: "Sure, here is the javascript code:",
+				}),
+				expect.objectContaining({
+					type: "CodeSnippet",
+					language: "javascript",
+					code: "print('Hello, world!')\n",
 				}),
 				expect.objectContaining({
 					type: "Paragraph",
-					text: "Green buds burst forth in rebirth",
+					text: "Final Paragraph.",
 				}),
-			])
-		);
-	});
+			]);
+		}
+	);
+
+	it.each(["\n", " "])(
+		"should map multiple paragraphs and code snippets (separated by %p)",
+		(separator: string) => {
+			const content = [
+				"Sure, here is the javascript code:",
+				"```javascript",
+				"print('Line one');\nprint('Line two');",
+				"```",
+				"Here is the typescript code:",
+				"```typescript",
+				"print('Line A');\nprint('Line B');",
+				"```",
+				"Final paragraph.",
+			].join(separator);
+
+			const result = mapToContentParts(content);
+
+			expect(result).toEqual([
+				expect.objectContaining({
+					type: "Paragraph",
+					text: "Sure, here is the javascript code:",
+				}),
+				expect.objectContaining({
+					type: "CodeSnippet",
+					language: "javascript",
+					code: "print('Line one');\nprint('Line two');\n",
+				}),
+				expect.objectContaining({
+					type: "Paragraph",
+					text: "Here is the typescript code:",
+				}),
+				expect.objectContaining({
+					type: "CodeSnippet",
+					language: "typescript",
+					code: "print('Line A');\nprint('Line B');\n",
+				}),
+				expect.objectContaining({
+					type: "Paragraph",
+					text: "Final paragraph.",
+				}),
+			]);
+		}
+	);
+
+	it.each(["\n", " "])(
+		"should clean up erroneous code markers (separated by %p)",
+		(separator: string) => {
+			const content = [
+				"Sure! Here is a poem about Spring:",
+				"```language",
+				"The sun warms the earth\nGreen buds burst forth in rebirth",
+				"```",
+			].join(separator);
+
+			const result = mapToContentParts(content);
+
+			expect(result).toEqual(
+				expect.arrayContaining([
+					expect.objectContaining({
+						type: "Paragraph",
+						text: "Sure! Here is a poem about Spring:",
+					}),
+					expect.objectContaining({
+						type: "Paragraph",
+						text: "The sun warms the earth",
+					}),
+					expect.objectContaining({
+						type: "Paragraph",
+						text: "Green buds burst forth in rebirth",
+					}),
+				])
+			);
+		}
+	);
 });
--- app/src/features/chat/mappers/contentMapper.ts
+++ app/src/features/chat/mappers/contentMapper.ts
@@ -1,87 +1,235 @@
 import { v4 as uuidv4 } from "uuid";
 
+type MapToPartFunction = (
+	content: string,
+	startIndex: number
+) => [MessagePart, number];
+
+const CODE_SNIPPET_MARKER = "```";
+
 export function mapToContentParts(content: string): MessagePart[] {
-	const parts = [];
-	const lines = content.split("\n");
+	const parts: MessagePart[] = [];
+	let index = 0;
+	while (isInBounds(content, index)) {
+		const char = content[index];
+		const type = getPartType(content, index);
+		const mapToPart = getMapToPartFunction(type);
+		const [part, offset] = mapToPart(content, index);
+		index += offset;
 
-	let i = 0;
-	while (i < lines.length) {
-		if (isNumberedPoint(lines[i])) {
-			const numberedPoints = extractNumberedPoints(
-				lines.filter((line, idx) => idx >= i)
-			);
-			parts.push({
-				type: "OrderedList",
-				listItems: numberedPoints.map((text) => {
-					return { id: uuidv4(), text };
-				}),
-			} as OrderedList);
-			i += numberedPoints.length;
+		while (isInBounds(content, index) && isSpaceOrNewLine(content[index])) {
+			index++;
 		}
 
-		if (isCodeMarker(lines[i])) {
-			const language = extractLanguage(lines[i]);
-			const codeLines = extractCodeLines(lines.filter((_, idx) => idx > i));
-			const code = codeLines.join("\n");
-
-			parts.push({
-				type: "CodeSnippet",
-				language,
-				code,
-			} as CodeSnippet);
-
-			i += codeLines.length + 2;
+		if (isErroneousCodeSnippet(part)) {
+			const paragraphs = mapCodeSnippetToParagraphs(part as CodeSnippet);
+			parts.push(...paragraphs);
+			continue;
 		}
 
-		if (i < lines.length) {
-			parts.push({
-				type: "Paragraph",
-				text: clean(lines[i]),
-			} as Paragraph);
-		}
-
-		i++;
+		parts.push(part);
 	}
 
 	return parts;
 }
 
-/* private functions */
-
-function isNumberedPoint(line: string): boolean {
-	return /^\d+\.\s/.test(line);
+function getPartType(content: string, startIndex: number): MessagePartType {
+	if (isCodeSnippetMarker(content, startIndex)) {
+		return "CodeSnippet";
+	}
+	if (isListItemNumber(content, startIndex)) {
+		return "OrderedList";
+	}
+	return "Paragraph";
 }
 
-function isCodeMarker(line: string): boolean {
-	return /^```(?:bash|c|cshtml|cpp|csharp|css|docker|fsharp|git|html|java|javascript|json|jsx|kotlin|lua|markdown|markup|python|rust|scala|swift|toml|tsx|typescript|yaml)/.test(
-		line
+function getMapToPartFunction(type: MessagePartType): MapToPartFunction {
+	if (type === "CodeSnippet") {
+		return mapCodeSnippet;
+	}
+	if (type === "OrderedList") {
+		return mapOrderedList;
+	}
+	return mapParagraph;
+}
+
+function mapParagraph(
+	content: string,
+	startIndex: number
+): [Paragraph, number] {
+	let offset = 0;
+	let text = "";
+	while (
+		isInBounds(content, startIndex + offset) &&
+		!isCodeSnippetMarker(content, startIndex + offset) &&
+		!isListItemNumber(content, startIndex + offset) &&
+		!isNewLine(content[startIndex + offset])
+	) {
+		text += content[startIndex + offset];
+		offset++;
+	}
+
+	return [
+		{
+			type: "Paragraph",
+			text: text.trim(),
+		},
+		offset,
+	];
+}
+
+function mapOrderedList(
+	content: string,
+	startIndex: number
+): [OrderedList, number] {
+	const listItems: ListItem[] = [];
+	let offset = 0;
+	while (
+		isInBounds(content, startIndex + offset) &&
+		isListItemNumber(content, startIndex + offset)
+	) {
+		let text = content.substring(startIndex + offset, startIndex + offset + 3);
+		offset += text.length;
+		while (
+			isInBounds(content, startIndex + offset) &&
+			!isCodeSnippetMarker(content, startIndex + offset) &&
+			!isListItemNumber(content, startIndex + offset) &&
+			!isNewLine(content[startIndex + offset])
+		) {
+			text += content[startIndex + offset];
+			offset++;
+		}
+
+		listItems.push({
+			id: uuidv4(),
+			text: text.trim(),
+		});
+
+		while (
+			isInBounds(content, startIndex + offset) &&
+			isNewLine(content[startIndex + offset])
+		) {
+			offset++;
+		}
+	}
+
+	return [
+		{
+			type: "OrderedList",
+			listItems,
+		},
+		offset,
+	];
+}
+
+function mapCodeSnippet(
+	content: string,
+	startIndex: number
+): [CodeSnippet, number] {
+	let offset = 0;
+	while (
+		isInBounds(content, startIndex + offset) &&
+		content[startIndex + offset] === "`"
+	) {
+		offset++;
+	}
+
+	let language = "";
+	while (
+		isInBounds(content, startIndex + offset) &&
+		!isSpace(content[startIndex + offset]) &&
+		!isNewLine(content[startIndex + offset])
+	) {
+		language += content[startIndex + offset];
+		offset++;
+	}
+
+	if (language.length === 0) {
+		language = "bash";
+	}
+
+	while (
+		isInBounds(content, startIndex + offset) &&
+		isSpaceOrNewLine(content[startIndex + offset])
+	) {
+		offset++;
+	}
+
+	let code = "";
+	while (
+		isInBounds(content, startIndex + offset) &&
+		!isCodeSnippetMarker(content, startIndex + offset)
+	) {
+		code += content[startIndex + offset];
+		offset++;
+	}
+
+	while (
+		isInBounds(content, startIndex + offset) &&
+		content[startIndex + offset] === "`"
+	) {
+		offset++;
+	}
+
+	code = code.trim();
+	if (!code.endsWith("\n")) {
+		code += "\n";
+	}
+
+	return [
+		{
+			type: "CodeSnippet",
+			language,
+			code,
+		},
+		offset,
+	];
+}
+
+function isErroneousCodeSnippet(messagePart: MessagePart): boolean {
+	return (
+		messagePart.type === "CodeSnippet" &&
+		(messagePart as CodeSnippet).language === "language"
 	);
 }
 
-function extractNumberedPoints(lines: string[]): string[] {
-	const numberedPoints: string[] = [];
-	let i = 0;
-	while (isNumberedPoint(lines[i])) {
-		numberedPoints.push(lines[i]);
-		i++;
-	}
-	return numberedPoints;
+function mapCodeSnippetToParagraphs({ code }: CodeSnippet): Paragraph[] {
+	return code
+		.split("\n")
+		.filter((text) => text.length > 0)
+		.map((text) => ({
+			type: "Paragraph",
+			text: text.trim(),
+		}));
 }
 
-function extractLanguage(line: string): string {
-	return line.replace("```", "").replace("language-", "").toLowerCase();
+function isInBounds(content: string, index: number): boolean {
+	return index < content.length;
 }
 
-function extractCodeLines(lines: string[]): string[] {
-	const codeLines: string[] = [];
-	let i = 0;
-	while (!lines[i].startsWith("```") && i < lines.length) {
-		codeLines.push(lines[i]);
-		i++;
-	}
-	return codeLines;
+function isCodeSnippetMarker(content: string, index: number): boolean {
+	return (
+		index + 2 < content.length &&
+		content.substring(index, index + 3) === CODE_SNIPPET_MARKER
+	);
 }
 
-function clean(line: string): string {
-	return line.replace("```language", "").replace("```", "");
+function isListItemNumber(content: string, index: number): boolean {
+	return (
+		index + 2 < content.length &&
+		!isNaN(Number(content[index])) &&
+		content.substring(index + 1, index + 3) === ". "
+	);
+}
+
+function isSpaceOrNewLine(char: string): boolean {
+	return isSpace(char) || isNewLine(char);
+}
+
+function isNewLine(char: string): boolean {
+	return char === "\n";
+}
+
+function isSpace(char: string): boolean {
+	return char === " ";
 }
