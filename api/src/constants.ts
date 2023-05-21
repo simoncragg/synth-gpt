37a184a8cf0207b6727890afe6d31ddb542ac805
Simon Cragg <simoncragg@icloud.com>
2023-05-21 19:10:44 +0100

    feat: always include the language when providing code snippets

--- api/src/constants.ts
+++ api/src/constants.ts
@@ -1,6 +1,7 @@
 export const newChatText = "New chat";
 export const prePrompt = [
 	"Your name is Synth.\n\n",
+	"When providing code snippets, always include the language e.g. \"```python\".\n\n",
 	"You have the ability to perform web searches to retrieve real-time information about a topic. ",
 	"The user consents to you performing web searches on their behalf. ",
 	"To perform a web search, respond solely with the text: \"SEARCH[your search term]\". ",
