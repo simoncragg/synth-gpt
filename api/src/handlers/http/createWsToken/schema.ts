132e44fc695c65bbde23172595eef87991f51eb8
Simon Cragg <simoncragg@icloud.com>
2023-06-07 21:02:26 +0100

    feat: add $schema property to remaining model schemas

--- api/src/handlers/http/createWsToken/schema.ts
+++ api/src/handlers/http/createWsToken/schema.ts
@@ -1,4 +1,5 @@
 export default {
+	$schema: "http://json-schema.org/draft-04/schema#",
 	type: "object",
 	properties: {
 		userId: { type: "string" }
--- api/src/handlers/http/generateTitle/schema.ts
+++ api/src/handlers/http/generateTitle/schema.ts
@@ -1,4 +1,5 @@
 export default {
+	$schema: "http://json-schema.org/draft-04/schema#",
 	type: "object",
 	properties: {
 		message: { type: "string" }
