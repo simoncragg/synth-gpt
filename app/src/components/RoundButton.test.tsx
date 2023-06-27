acc9117b50d9adac1c4efdc0dfd9602784ee8e03
Simon Cragg <simoncragg@icloud.com>
2023-06-27 20:22:25 +0100

    chore: remove rogue comment and apply Prettier formatting

--- app/src/components/RoundButton.test.tsx
+++ app/src/components/RoundButton.test.tsx
@@ -3,18 +3,24 @@ import { RoundButton } from "./RoundButton";
 
 describe("RoundButton component", () => {
 	it("should render with aria-label and children props", () => {
-		const { getByLabelText, getByText } = render(<RoundButton ariaLabel="Test Button" onClick={() => {}}>Click me</RoundButton>);
+		const { getByLabelText, getByText } = render(
+			<RoundButton ariaLabel="Test Button" onClick={() => {}}>
+				Click me
+			</RoundButton>
+		);
 		expect(getByLabelText("Test Button")).toBeInTheDocument();
 		expect(getByText("Click me")).toBeInTheDocument();
 	});
 
 	it("should call onClick prop when clicked", () => {
 		const handleClick = jest.fn();
-		const { getByRole } = render(<RoundButton ariaLabel="Test Button" onClick={handleClick}>Click me</RoundButton>);
+		const { getByRole } = render(
+			<RoundButton ariaLabel="Test Button" onClick={handleClick}>
+				Click me
+			</RoundButton>
+		);
 		const button = getByRole("button");
 		fireEvent.click(button);
 		expect(handleClick).toHaveBeenCalledTimes(1);
 	});
-
-	// Add more tests as necessary
 });
