3b0a62ccecd5d27d7a746db4fbe4efe19b6d2e8f
Simon Cragg <simoncragg@icloud.com>
2023-08-15 23:44:53 +0100

    refactor: clean up test

--- app/src/components/Code.test.tsx
+++ app/src/components/Code.test.tsx
@@ -8,22 +8,16 @@ Object.assign(navigator, {
 });
 
 describe("Code component", () => {
-	it("should copy code to clipboard when 'Copy code' button is clicked", async () => {
-
-		const code = "console.log(\"Hello, world!\")";
-		const language = "javascript";
-		
-		const { getByText } = render(<Code code={code} language={language} />);
+	it("should copy code to clipboard when 'Copy code' button is clicked", () => {
+		const code = "console.log('Hello, world!')";
+		const { getByText } = render(<Code code={code} language="javascript" />);
 		const copyButton = getByText("Copy code");
-
 		const clipboardSpy = jest.spyOn(navigator.clipboard, "writeText");
 		clipboardSpy.mockResolvedValueOnce();
 
-		// Act
 		fireEvent.click(copyButton);
 
 		expect(clipboardSpy).toHaveBeenCalledWith(code);
-		await expect(getByText("Copied")).toBeInTheDocument();
+		expect(getByText("Copied")).toBeInTheDocument();
 	});
 });
-
